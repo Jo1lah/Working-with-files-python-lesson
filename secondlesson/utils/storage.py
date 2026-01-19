0921c93f5c29e79fd2b683c03cab0cecca2a6247
Jo1lah <ershov.rus00@gmail.com>
2026-01-19 23:39:38 +0500

    Lessons works

---
+++ secondlesson/utils/storage.py
@@ -0,0 +1,13 @@
+import json
+
+PATH = "data/notes.json"
+
+def load_notes():
+    with open(PATH, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+def save_notes(notes):
+    with open(PATH, "w", encoding="utf-8") as file:
+        json.dump(notes, file, indent=2,ensure_ascii=False)
+
+
